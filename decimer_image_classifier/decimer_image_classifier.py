9af54ddd9280d8d0a3d20e54816fcb488f95030e
Cordelia Cavill <159061167+cmc227@users.noreply.github.com>
2024-06-30 13:17:14 +0100

    Update decimer_image_classifier.py

--- decimer_image_classifier/decimer_image_classifier.py
+++ decimer_image_classifier/decimer_image_classifier.py
@@ -11,6 +11,7 @@ class DecimerImageClassifier:
     def __init__(self):
         # Establish GPU growth, load default GPU
         gpus = tf.config.experimental.list_physical_devices("GPU")
+        print('from script decimer_image_classifier, the gpus are: {f}')
         for gpu in gpus:
             tf.config.experimental.set_memory_growth(gpu, True)
         # Load model
